6abdc7e9d660f8c01e2923016698b2faa4c9176e
Richard Mands <richard@eduku.org>
2017-03-22 17:02:42 +0800

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var findMissing = function (list) {
   let diffArr = []
   let diff = 0;
   let num = 0;
+  let position = 0;
 
   for (var i = 0; i < list.length-1; i++) {
     diff=list[i] - list[i+1]
@@ -16,18 +17,27 @@ var findMissing = function (list) {
       num = diffArr[i]
       break;
     }
-    if (diffArr[i] == diffArr[i+1] && diffArr[i] > 0){
+    else if (diffArr[i] == diffArr[i+1] && diffArr[i] > 0){
       type = 'subtract'
       num = diffArr[i]
       break;
     }
-    if ((list[i+1] / list[i]) == (list[i+2] / list[i+1])){
+    else if ((list[i+1] / list[i]) == (list[i+2] / list[i+1])){
       type = 'multiply'
       num = list[i+1] / list[i]
       break;
     }
-
+    else if ((list[i+1] * list[i]) == (list[i+2] * list[i+1])){
+      type = 'divide'
+      num = list[i+1] / list[i]
+      break;
+    }
   }
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
+
   console.log('diffArr: ', diffArr)
   console.log('type: ', type);
   console.log('num: ', num)
