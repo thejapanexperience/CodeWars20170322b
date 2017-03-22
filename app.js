2e74bd42e55a6eb568962bc8d562a06f56c8d0fe
Richard Mands <richard@eduku.org>
2017-03-22 20:24:14 +0800

    update

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
-var findMissing = function (list) {
-  let diffArr = []
-  let diff = 0;
-  let num = 0;
-  let final = 0;
-
-  for (var i = 0; i < list.length-1; i++) {
-    diff=list[i] - list[i+1]
-    diffArr.push(diff)
-  }
-
-  for (let i = 0; i < diffArr.length; i++) {
-      if (Math.abs(diffArr[0]) > Math.abs(diffArr[1])){
-        num = diffArr[1]
-      } else {
-        num = diffArr[0]
-      }
-  }
-
-if (diffArr[0] < 0){
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] + Math.abs(num) !== list[i+1]){
-      final = list[i] + Math.abs(num)
-      return final;
-    }
+var recoverSecret = function(triplets) {
+  console.log('triplets: ', triplets)
+  let secret = []
+  secret.push(triplets[i][0]);
+  console.log('secret: ', secret)
+  for (var i = 0; i < triplets.length; i++) {
   }
 }
 
-if (diffArr[0] > 0){
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] - Math.abs(num) !== list[i+1]){
-      final = list[i] - Math.abs(num)
-      return final;
-    }
-  }
-}
+let triplets = [
+  ['t', 'u', 'p'],
+  ['w', 'h', 'i'],
+  ['t', 's', 'u'],
+  ['a', 't', 's'],
+  ['h', 'a', 'p'],
+  ['t', 'i', 's'],
+  ['w', 'h', 's']
+];
 
-};
-
-findMissing([3,6,9,12,18, 21]);
-// findMissing([1,3,5,9,11]);
+recoverSecret(triplets)
