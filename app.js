58956cfb833d2840350faa2e28fd118ab756970c
Richard Mands <richard@eduku.org>
2017-03-22 17:23:45 +0800

    update

--- app.js
+++ app.js
@@ -15,33 +15,38 @@ var findMissing = function (list) {
     if (diffArr[i] == diffArr[i+1] && diffArr[i] < 0){
       type = 'add'
       num = diffArr[i]
-      break;
     }
     else if (diffArr[i] == diffArr[i+1] && diffArr[i] > 0){
       type = 'subtract'
       num = diffArr[i]
-      break;
     }
-    else if ((list[i+1] / list[i]) == (list[i+2] / list[i+1])){
-      type = 'multiply'
-      num = list[i+1] / list[i]
-      break;
-    }
-    else if ((list[i+1] * list[i]) == (list[i+2] * list[i+1])){
-      type = 'divide'
-      num = list[i+1] / list[i]
-      break;
-    }
+    // if (diffArr.length > 1 && )
+    // else if ((list[i+1] / list[i]) == (list[i+2] / list[i+1])){
+    //   type = 'multiply'
+    //   num = list[i+1] / list[i]
+    //   break;
+    // }
+    // else if ((list[i+1] * list[i]) == (list[i+2] * list[i+1])){
+    //   type = 'divide'
+    //   num = list[i+1] / list[i]
+    //   break;
+    // }
   }
 
-for (var i = 0; i < array.length; i++) {
-  array[i]
+for (var i = 1; i < diffArr.length; i++) {
+  if (diffArr[i] !== diffArr[0]){
+    position = [i + 1]
+  }
 }
 
+list.splice(position, 0, list[position] + num)
   console.log('diffArr: ', diffArr)
   console.log('type: ', type);
   console.log('num: ', num)
+  console.log('position: ', position)
+  console.log('list: ', list)
   return list [0] ;
 };
 
-findMissing([1,3,5,9,11]);
+findMissing([3,6,9,12,18, 21]);
+// findMissing([1,3,5,9,11]);
