ba433c535005de4be0332d4c0cf20c25d667e33a
Richard Mands <richard@eduku.org>
2017-03-22 16:38:28 +0800

    update

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 var findMissing = function (list) {
   console.log('list: ', list);
-  let type = {
-    1: 'add',
-    2: 'subtract',
-    3: 'multiply',
-    4: 'divide'
-  }
+  let type;
   if (list[0] - list[1] === list[1] - list[2] && list[0 - list[1] > 0 ] ){
-    type = 'add'
+    type = 'add';
   }
 
-  console.log('type: ', type)
+  console.log('type: ', type);
   return list [0] ;
-}
+};
 
-findMissing([1,3,5,9,11])
+findMissing([1,3,5,9,11]);
