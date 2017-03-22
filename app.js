e7df2d6355550dfe8b403f5ab6f69a349fda1ab8
Richard Mands <richard@eduku.org>
2017-03-22 16:53:34 +0800

    update

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ var findMissing = function (list) {
   console.log('list: ', list);
   let type = '';
   let diffArr = []
-  let diff=0;
+  let diff = 0;
+  let num = 0;
 
-  if (list[0] - list[1] === list[1] - list[2] && list[0] - list[1] < 0  ){
-    type = 'add';
-  }
   for (var i = 0; i < list.length-1; i++) {
     diff=list[i] - list[i+1]
     diffArr.push(diff)
+  }
+
+  for (var i = 0; i < diffArr.length; i++) {
+    if (difArr[i] == difArr[i+1] && difArr[i] < 0){
+      type = 'add'
+      break;
+    }
+    if (difArr[i] == difArr[i+1] && difArr[i] > 0){
+      type = 'subtract'
+      break;
+    }
+    if ((difArr[i+1] / difArr[i]) == (difArr[i+2] / difArr[i+1])){
+      type = 'multiply'
+      num
+      break;
+    }
+
   }
   console.log('diffArr: ', diffArr)
   console.log('type: ', type);
