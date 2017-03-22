4773017f8f2bc70a06c6588c00c165a59b364b2b
Richard Mands <richard@eduku.org>
2017-03-22 21:31:45 +0800

    update

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var recoverSecret = function(triplets) {
   console.log('triplets: ', triplets)
-  let secret = []
-  secret.push(triplets[i][0]);
-  console.log('secret: ', secret)
-  for (var i = 0; i < triplets.length; i++) {
+  let secret = [];
+  let letters = {};
+  let length = 0;
+  let rules = []
+  console.log('triplets[0]: ', triplets[0])
+
+  for (let i = 0; i < triplets.length; i++) {
+    let triplet = [...triplets[i]]
+    let letter = '';
+    for (let j = 0; j < triplet.length; j++) {
+      letter = triplet[j]
+      if (!letters[letter]){
+        length += 1;
+      }
+      letters[letter] = [];
+      }
   }
+
+  console.log('length: ', length)
+
+  for (let i = 0; i < triplets.length; i++) {
+    let triplet = [triplets[i]]
+    for (let j = 0; j < triplet.length; j++) {
+      console.log(': triplet[j]', triplet[j])
+      letters[triplet[j]][0].push
+    }
+  }
+  console.log('letters: ', letters)
 }
 
 let triplets = [
