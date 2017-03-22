af474909807562e72a5589ad09593eab7bb0eba2
Richard Mands <richard@eduku.org>
2017-03-22 18:05:59 +0800

    update

--- app.js
+++ app.js
@@ -1,61 +1,45 @@
 var findMissing = function (list) {
-  console.log('list: ', list);
-  let type = '';
   let diffArr = []
   let diff = 0;
   let num = 0;
-  let position = 0;
+  let final = 0;
+  console.log('list: ', list)
 
   for (var i = 0; i < list.length-1; i++) {
     diff=list[i] - list[i+1]
     diffArr.push(diff)
   }
 
-  for (var i = 0; i < diffArr.length; i++) {
+  for (let i = 0; i < diffArr.length; i++) {
     if (diffArr[i] == diffArr[i+1]){
-      type = 'add'
       num = diffArr[i]
-      console.log('num: ', num)
     } else if (diffArr.length === 2){
-      if (diffArr[0]> diffArr[1]){
+      if (Math.abs(diffArr[0]) > Math.abs(diffArr[1])){
         num = diffArr[1]
       } else {
         num = diffArr[0]
       }
     }
   }
-  //
-  // for (var i = 0; i < diffArr.length; i++) {
-  //   if (diffArr[i] == diffArr[i+1] && diffArr[i] < 0){
-  //     type = 'add'
-  //     num = diffArr[i]
-  //     console.log('num: ', num)
-  //   }
-  //   else if (diffArr[i] == diffArr[i+1] && diffArr[i] > 0){
-  //     type = 'subtract'
-  //     num = diffArr[i]
-  //     console.log('num: ', num)
-  //   }
-  // }
+console.log('diffArr: ', diffArr)
+console.log('num: ', num);
 
-for (var i = 1; i < diffArr.length; i++) {
-  if ( diffArr[i] !== diffArr[0]){
-    position = [i + 1]
-    list.splice(position, 0, list[position] + num)
+if (diffArr[0] > 0){
+  for (let i = 1; i < list.length; i++) {
+    if ( list[i] + num !== list[i+1]){
+      final = list[i] + num
+    }
   }
-  // else if (type === 'add' && diffArr[i] !== diffArr[0]){
-  //   position = [i + 1]
-  //   list.splice(position, 0, list[position] - num)
-  // }
 }
-  //
-  // console.log('diffArr: ', diffArr)
-  // console.log('type: ', type);
-  console.log('num: ', num)
-  console.log('position: ', position)
-  // console.log('list: ', list)
-  // console.log('list[position]: ', list[position])
-  return list [position] ;
+if (diffArr[0] < 0){
+  for (let i = 1; i < list.length; i++) {
+    if ( list[i] - num !== list[i+1]){
+      final = list[i] + num
+    }
+  }
+}
+console.log('final: ', final);
+return final;
 };
 
 findMissing([3,6,9,12,18, 21]);
